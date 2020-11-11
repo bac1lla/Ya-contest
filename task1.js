59ccf0024da247b21a70050bf218e6061023363d
Vasilyev Alexander <2365434@mail.ru>
2020-11-12 02:14:30 +0300

    add task1 & task2

---
+++ task1.js
@@ -0,0 +1,46 @@
+const explorers = [
+    ["Mallory", "Everest", "Mont Blanc", "Pillar Rock"],
+    ["Mawson", "South Pole", "New Hebrides"],
+    ["Hillary", "Everest", "South Pole"]
+]
+
+
+// expected output
+// [
+//  ["Everest", "Hillary", "Mallory"],
+//  ["South Pole", "Hillary", "Mawson"],
+//  ["Mont Blanc", "Mallory"],
+//  ["Pillar Rock", "Mallory"],
+//  ["New Hebrides", "Mawson"]
+// ]
+
+//также есть функция flat()
+const flatConcat = arr => [].concat(...arr)
+
+//разверачиваем массив и удаляем исследователей
+toponym = flatConcat(explorers.map( (string) => {
+    return string.filter((item, index) => index !== 0)
+}))
+
+
+// удаляем дубликаты и обораичваем топонимы в массивы
+toponym = toponym.filter( (toponym, index, array) => {
+    return array.indexOf(toponym) === index
+}).map(toponym => [toponym]);
+
+
+toponym.forEach((top) => {
+
+    explorers.map(string => {
+        if (string.includes(top[0])) {
+            top.push(string[0]);
+        }
+    })
+})
+
+console.log(toponym)
+
+
+
+
+
---
+++ task2.js
@@ -0,0 +1,67 @@
+const input1 = [
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [0, 1],
+    [1, 1],
+    [1, 1],
+    [0, 0],
+    [1, 0]
+]
+
+const input2 = [
+    [1, 0],
+    [1, 1],
+    [0, 1],
+    [1, 0]
+]
+
+function validateData(log) {
+    // let path = log.reduce((acc = [], value ) => {
+    //     acc[0] += value[0];
+    //     acc[1] += value[1];
+    //     return acc
+    // })
+    // return path[0] % 2 === 0 && path[1] % 2 === 0
+
+    let pathX = 0;
+    let pathY = 0;
+
+    for (let diff of log) {
+        pathX += diff[0] || 0;
+        pathY += diff[1] || 0
+    }
+
+    return pathX % 2 === 0 && pathY % 2 === 0
+}
+
+function pathFinder(log) {
+
+    if (validateData(log) === false) {
+        return null;
+    }
+
+    let diffY = 0;
+    let diffX = 0
+    for (let diff of log) {
+        if ( diff[0] === 1 && diffX === 0) {
+            diffX--;
+            diff[0] *= -1;
+        } else if (diff[0] === 1 && diffX === -1) {
+            diffX++;
+        }
+        if (diff[1] === 1 && diffY === 0) {
+            diffY--;
+            diff[1] *= -1;
+        } else if(diff[1] === 1 && diffY === -1) {
+            diffY++
+        }
+    }
+
+    return log
+}
+
+
+// console.log(input1)
+console.log(pathFinder(input1))
+console.log(pathFinder(input2))
